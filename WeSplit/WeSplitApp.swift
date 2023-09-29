0b088b0b3d844c646df35499963e0b90145673d1
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-09-29 23:58:34 +0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shadat Vai on 29/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
